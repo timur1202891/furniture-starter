e321b7d61551ca52ed009646585e00be74937792
timur1202891 <timur1202891@gmail.com>
2023-04-30 21:47:17 +0500

    add files to githuh

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import { products } from '../data';
+import ProductSlider from '../components/ProductSlider';
 const Products = () => {
-  return <div>Products</div>;
+  const { title, subtitle } = products;
+  return (
+    <section className='section text-center'>
+      <div className='container mx-auto'>
+        <div>
+          <h2 className='title'>{title}</h2>
+          <p className='max-w-[639px] mx-auto mb-[50px] lg:mb-[70px]'>{subtitle}</p>
+        </div>
+        <ProductSlider />
+      </div>
+    </section>
+  );
 };
 
 export default Products;
